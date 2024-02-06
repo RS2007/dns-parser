5c8d739e4e6c21e8ef40ca3cef7e292ee900d838
Rohith <rohithsuresh207@gmail.com>
2024-02-06 11:51:59 +0530

    Answer packet parsing working

--- dns.h
+++ dns.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <cstring>
 #include <endian.h>
 #include <iostream>
@@ -17,7 +18,7 @@ typedef struct __attribute__((packed)) {
   uint16_t additional_count;
 } DNS_Header_t;
 
-DNS_Header_t *parse_header(const char *bytes) { return (DNS_Header_t *)bytes; }
+DNS_Header_t *parse_header(char *bytes) { return (DNS_Header_t *)bytes; }
 
 /* |                                               | */
 /* /                     QNAME                     /
@@ -34,11 +35,19 @@ typedef struct {
   uint16_t q_class;
 } DNS_Question_t;
 
-size_t parse_question(const char *bytes, DNS_Question_t *question) {
+typedef struct {
+  char *name;
+  size_t len;
+} dns_name_string;
+
+dns_name_string *get_name(char *bytes, char *orig_buffer) {
   size_t i = 0;
   size_t len;
   std::vector<std::string> names;
-  const char *q_start = bytes + sizeof(DNS_Header_t);
+  char *q_start = (char *)bytes;
+  if ((q_start[i] & 0b11000000) == 0b11000000) {
+    q_start = orig_buffer + (htobe16((*(uint16_t *)q_start)) & 0x3FFF);
+  }
   len = q_start[i];
   while (q_start[++i] != (char)0) {
     std::string name;
@@ -59,19 +68,100 @@ size_t parse_question(const char *bytes, DNS_Question_t *question) {
   char *domain_name = (char *)malloc(domain_name_str.size());
   strncpy(domain_name, domain_name_str.c_str(), domain_name_str.size());
   domain_name[domain_name_str.size()] = '\0';
-  question->q_name = domain_name;
+  auto dns_name = (dns_name_string *)malloc(sizeof(dns_name_string));
+  dns_name->name = domain_name;
+  dns_name->len = i;
+  return dns_name;
+}
+
+size_t parse_question(char *bytes, DNS_Question_t *question,
+                      char *orig_buffer) {
+  auto dns_name = get_name(bytes, orig_buffer);
+  question->q_name = dns_name->name;
   typedef struct __attribute__((packed)) {
     uint16_t q_type;
     uint16_t q_class;
   } q_type_class_t;
-  auto q_type_class = (q_type_class_t *)(q_start + i);
+  auto q_type_class = (q_type_class_t *)(bytes + dns_name->len);
   question->q_type = htobe16(q_type_class->q_type);
   question->q_class = htobe16(q_type_class->q_class);
-  return i + 4;
+  return dns_name->len + 4;
 }
-
 static std::string dns_type_strings[] = {
-    "NULL", "A",    "NS",  "MD",  "MF",    "CNAME", "SOA", "MB", "MG",
-    "MR",   "NULL", "WKS", "PTR", "HINFO", "MINFO", "MX",  "TXT"};
+    "INVALID", "A",    "NS",  "MD",  "MF",    "CNAME", "SOA", "MB", "MG",
+    "MR",      "NULL", "WKS", "PTR", "HINFO", "MINFO", "MX",  "TXT"};
 
-static std::string dns_class_strings[] = {"NULL", "IN", "CS", "CH", "HS"};
+static std::string dns_class_strings[] = {"INVALID", "IN", "CS", "CH", "HS"};
+
+static std::string dns_resource_type_strings[] = {
+    "INVALID", "A",    "NS",  "MD",  "MF",    "CNAME", "SOA", "MB",  "MG",
+    "MR",      "NULL", "WKS", "PTR", "HINFO", "MINFO", "MX",  "TXT",
+};
+
+typedef enum : uint16_t {
+  INVALID,
+  A,
+  NS,
+  MD,
+  MF,
+  CNAME,
+  SOA,
+  MB,
+  MG,
+  MR,
+  NULLE,
+  WKS,
+  PTR,
+  HINFO,
+  MINFO,
+  MX,
+  TXT,
+} RRTYPE;
+
+static std::string dns_resource_class_strings[] = {"INVALID", "IN", "CS", "CH",
+                                                   "HS"};
+
+typedef struct {
+  char *r_name;
+  uint16_t r_type;
+  uint16_t r_class;
+  uint32_t r_ttl;
+  uint16_t r_rdlength;
+  char *r_data;
+} DNS_Resource_t;
+
+inline void parse_answer(char *bytes, DNS_Resource_t *answer,
+                         char *orig_buffer) {
+  auto dns_name = get_name(bytes, orig_buffer);
+  answer->r_name = dns_name->name;
+  typedef struct __attribute__((packed)) {
+    RRTYPE r_type;
+    uint16_t r_class;
+    uint32_t r_ttl;
+    uint16_t r_rdlength;
+  } fixed_size_packet;
+  auto f_size_packet = (fixed_size_packet *)(bytes + 2);
+  answer->r_class = htobe16(f_size_packet->r_class);
+  answer->r_type = htobe16(f_size_packet->r_type);
+  answer->r_ttl = htobe32(f_size_packet->r_ttl);
+  answer->r_rdlength = htobe16(f_size_packet->r_rdlength);
+  switch (answer->r_type) {
+  case A: {
+    uint32_t ipAddress = htobe32(*(uint32_t*)(bytes+2+sizeof(fixed_size_packet)));
+    unsigned char octet1, octet2, octet3, octet4;
+
+    octet1 = (ipAddress >> 24) & 0xFF;
+    octet2 = (ipAddress >> 16) & 0xFF;
+    octet3 = (ipAddress >> 8) & 0xFF;
+    octet4 = ipAddress & 0xFF;
+    char* ip_addr = (char*)malloc(16);
+    snprintf(ip_addr,16,"%d.%d.%d.%d",octet1,octet2,octet3,octet4);
+    answer->r_data = ip_addr;
+    break;
+  }
+  default: {
+    assert(0 && "Not implemented");
+  }
+  }
+  return;
+};
--- dns_test.cpp
+++ dns_test.cpp
@@ -1,5 +1,6 @@
 #include "dns.h"
 #include <cstring>
+#include <endian.h>
 #include <iostream>
 #include <vector>
 
@@ -16,19 +17,28 @@ std::vector<char> HexToBytes(const std::string &hex) {
 }
 
 void test_dns_header_parse() {
-  std::vector<char> bytes1 =
-      HexToBytes("4c76010000010000000000010c74696d65736f66696e6469610a696e64696"
-                 "174696d657303636f6d00000100010000290200000000000000");
+  std::vector<char> bytes1 = HexToBytes(
+      "a0208180000100010000000105666f6e74730a676f6f676c656170697303636f6d000001"
+      "0001c00c00010001000000ae00048efab64a0000290200000000000000");
   /* std::vector<char> bytes1 =
    * HexToBytes("ec2e98e9046b34e894fa3f5e08004500006b030400003e11f7c4c0a80001c0a80068003581c300577fa37baf818000010001000000010f7a2d7034322d696e7374616772616d046331307209696e7374616772616d03636f6d0000010001c00c000100010000000c00049df017ae0000290200000000000000");
    */
   std::string str = std::string(bytes1.begin(), bytes1.end());
-  DNS_Header_t *dns_header = parse_header(str.c_str());
+  DNS_Header_t *dns_header = parse_header((char *)str.c_str());
+  uint16_t RESERVED_FLAG = 0b0000000001000000;
+  if ((dns_header->flags & RESERVED_FLAG) != 0) {
+    printf("Error:Invalid packet");
+  }
+
   std::cout << "Header is: " << std::hex << htobe16(dns_header->id) << "\n";
   std::cout << "Question num is: " << std::hex
             << htobe16(dns_header->question_count) << "\n";
   std::cout << "Answer num is: " << std::hex
             << htobe16(dns_header->answer_count) << "\n";
+  std::cout << "Nameserver num is: " << htobe16(dns_header->authorities_count)
+            << "\n";
+  std::cout << "Additional num is: " << htobe16(dns_header->additional_count)
+            << "\n";
   if (htobe16(dns_header->question_count) != 1) {
     std::cout << "Invalid packet"
               << "\n";
@@ -36,15 +46,36 @@ void test_dns_header_parse() {
               << "\n";
     return;
   }
-  for (size_t i = 0; i < htobe16(dns_header->question_count); i++) {
+  size_t qn_count = htobe16(dns_header->question_count);
+  size_t tot_qn_size = 0;
+  for (size_t i = 0; i < qn_count; i++) {
     DNS_Question_t *dns_question =
         (DNS_Question_t *)malloc(sizeof(DNS_Question_t));
-    size_t qn_size = parse_question(str.c_str(), dns_question);
+    size_t qn_size =
+        parse_question((char *)str.c_str() + sizeof(DNS_Header_t) + tot_qn_size,
+                       dns_question, (char *)str.c_str());
+    tot_qn_size += qn_size;
     std::cout << "Question domain name is: " << dns_question->q_name << "\n";
     std::cout << "Question type is: " << dns_type_strings[dns_question->q_type]
               << "\n";
     std::cout << "Question class is: "
               << dns_class_strings[dns_question->q_class] << "\n";
+  }
+  std::cout << "Tot qn size = " << std::dec << tot_qn_size << "\n";
+  std::cout << "Tot qn size + header size = " << std::dec
+            << tot_qn_size + sizeof(DNS_Header_t) << "\n";
+  for (size_t i = 0; i < htobe16(dns_header->answer_count); i++) {
+    auto dns_answer = (DNS_Resource_t *)malloc(sizeof(DNS_Resource_t));
+    parse_answer((char *)str.c_str() + sizeof(DNS_Header_t) + tot_qn_size,
+                 dns_answer, (char *)str.c_str());
+    std::cout << "Answer name = " << dns_answer->r_name << "\n";
+    std::cout << "Answer type = "
+              << dns_resource_type_strings[dns_answer->r_type] << "\n";
+    std::cout << "Answer class = "
+              << dns_resource_class_strings[dns_answer->r_class] << "\n";
+    std::cout << "Answer rdlength = " << dns_answer->r_rdlength << "\n";
+    std::cout << "Answer ttl = " << dns_answer->r_ttl << "\n";
+    std::cout << "Answer data = " << dns_answer->r_data << "\n";
   }
 }
 
